093ed1cee949b122a3146e5ceab1df58d924a05c
Savannah Wilding <savannahwilding@outlook.com>
2024-01-22 16:56:14 +0000

    change team name

--- src/main/java/com/thg/accelerator23/connectn/ai/whatagame/WhatAGame.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/whatagame/WhatAGame.java
@@ -1,14 +1,12 @@
-package com.thg.accelerator23.connectn.ai.yourteam;
-
+package com.thg.accelerator23.connectn.ai.whatagame;
 import com.thehutgroup.accelerator.connectn.player.Board;
 import com.thehutgroup.accelerator.connectn.player.Counter;
 import com.thehutgroup.accelerator.connectn.player.Player;
 
-
-public class NameAi extends Player {
-  public NameAi(Counter counter) {
+public class WhatAGameAi extends Player {
+  public WhatAGameAi(Counter counter) {
     //TODO: fill in your name here
-    super(counter, NameAi.class.getName());
+    super(counter, WhatAGameAi.class.getName());
   }
 
   @Override
